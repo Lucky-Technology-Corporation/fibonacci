f81ffe7f7f36d6d3c80d24b25060344ead3dd29d
Adam Barr-Neuwirth <6325943+slugiscool99@users.noreply.github.com>
2023-08-07 14:59:14 -0700

    fix

--- src/MainContent/Database/DatabaseRow.tsx
+++ src/MainContent/Database/DatabaseRow.tsx
@@ -63,7 +63,7 @@ export default function DatabaseRow({collection, keys, data, rowKey, setShouldSh
                 <td className={`font-mono p-1 border-none ${(editing == key) ? "bg-[#383842]" : ""}`} key={`${rowKey}-${index+1}`}>
                     <input 
                         type="text" 
-                        className={`w-full bg-transparent border-0 outline-0 text-xs ${shouldShowStrikethrough ? "line-through" : ""} ${(rowValues[key].toString() || "").startsWith("https://") && shouldBlockEdits ? "cursor-pointer" : ""}`} 
+                        className={`w-full bg-transparent border-0 outline-0 text-xs ${shouldShowStrikethrough ? "line-through" : ""} ${((rowValues[key] || "").toString()).startsWith("https://") && shouldBlockEdits ? "cursor-pointer" : ""}`} 
                         onFocus={() => setupEditing(key)}
                         value={(editing == key) ? pendingInputValue : rowValues[key]}
                         onClick={() => {
