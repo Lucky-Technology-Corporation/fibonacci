704ace8dfa3b2ecd5965b6aab2900dd9d6cd4773
nadialatifi2 <nlatifi420@berkeley.edu>
2023-08-09 17:55:50 -0700

    start of analytics

---
+++ src/API/MonitoringAPI.tsx
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { useContext } from 'react';
+import { useAuthHeader } from 'react-auth-kit';
+import { SwizzleContext } from '../Utilities/GlobalContext';
+
+const BASE_URL = 'http://localhost:4000/api/v1';
+
+export default function useApi() {
+    const authHeader = useAuthHeader();
+    const { activeProject } = useContext(SwizzleContext);
+
+    const getMonitoring = async () => {
+        try {
+            if (!activeProject) {
+                throw new Error("No active project selected");
+            }
+
+            const response = await axios.get(`${BASE_URL}/projects/${activeProject}/monitoring`, {
+                headers: {
+                    Authorization: authHeader(),
+                },
+                params: { project_id: activeProject }, // Pass the project_id as a query parameter
+            });
+
+            return response.data;
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
+    };
+
+    return {
+        getMonitoring,
+    };
+}
---
+++ src/MainContent/MonitoringPage.tsx
@@ -0,0 +1,50 @@
+import { Card, Title, LineChart } from "@tremor/react";
+
+const chartdataax = [
+  {
+    year: 1970,
+    "Export Growth Rate": 2.04,
+    "Import Growth Rate": 1.53,
+  },
+  {
+    year: 1971,
+    "Export Growth Rate": 1.96,
+    "Import Growth Rate": 1.58,
+  },
+  {
+    year: 1972,
+    "Export Growth Rate": 1.96,
+    "Import Growth Rate": 1.61,
+  },
+  {
+    year: 1973,
+    "Export Growth Rate": 1.93,
+    "Import Growth Rate": 1.61,
+  },
+  {
+    year: 1974,
+    "Export Growth Rate": 1.88,
+    "Import Growth Rate": 1.67,
+  },
+  //...
+];
+
+const dataFormatter = (number: number) => `${Intl.NumberFormat("us").format(number).toString()}`;
+
+const MyLineChart = () => (
+  <Card className = "h-72">
+    <Title>Export/Import Growth Rates (1970 to 2021)</Title>
+    <LineChart
+      className="h-72"
+      data={chartdataax}
+      index="year"
+      categories={["Export Growth Rate", "Import Growth Rate"]}
+      colors={["emerald", "gray"]}
+      valueFormatter={dataFormatter}
+      yAxisWidth={40}
+      theme="dark"
+    />
+  </Card>
+);
+
+export default MyLineChart;
