7abaef899ac4f804290540d52e58e42ac5ca8e5e
Adam Barr-Neuwirth <6325943+slugiscool99@users.noreply.github.com>
2023-10-05 09:52:37 -0700

    fix appcodepage

--- src/MainContent/AppCode/AppCodePage.tsx
+++ src/MainContent/AppCode/AppCodePage.tsx
@@ -117,16 +117,19 @@ export default function AppCodePage() {
   return (
     <div className="h-full overflow-scroll min-h-[50vh]">
       <div className={`flex-1 pr-2 mx-4 mb-4 mt-1 text-lg flex justify-between`}>
-        <div>
-          <div className={`font-bold text-base`}>
-            Frontend
-            <div className="m-auto ml-2 inline-flex items-center rounded-md bg-yellow-300 bg-opacity-30 px-2 mt-1 py-0.5 text-xs font-medium text-yellow-300 ring-1 ring-inset ring-yellow-300/20">
-              Beta
+        <div className="flex w-full">
+          <div>
+            <div className={`font-bold text-base`}>
+              Frontend
+              <div className="m-auto ml-2 inline-flex items-center rounded-md bg-yellow-300 bg-opacity-30 px-2 mt-1 py-0.5 text-xs font-medium text-yellow-300 ring-1 ring-inset ring-yellow-300/20">
+                Beta
+              </div>
             </div>
+            <div className={`text-sm mt-0.5`}>Click any request to see its logs</div>
           </div>
 
           <Dropdown
-            className="my-auto"
+            className="my-auto ml-auto mr-2"
             onSelect={(item: any) => {
                 if(item != "swift"){
                     toast.error("Multi language code generation is in alpha. Your account only has access to beta or higher.")
@@ -139,9 +142,9 @@ export default function AppCodePage() {
             title={languages.find((language: any) => language.id == selectedMethod)?.name}
             />
         </div>
-       
-  
-        <div className="pr-2 mr-4 pl-4 pt-1 flex flex-row space-x-2">
+      </div>
+
+      <div className="pr-2 mr-4 pl-4 pt-1 flex flex-row space-x-2">
           <table className="w-full">
             <thead className="bg-[#85869822]">
               <tr className="border-b border-[#4C4F6B]">
@@ -172,58 +175,6 @@ export default function AppCodePage() {
                 </tr>
             </tbody>
           </table>
-        </div>
-
-        <Dropdown
-          className="my-auto"
-          onSelect={(item: any) => {
-            if (item != "swift") {
-              toast.error(
-                "Multi language code generation is in alpha. Your account only has access to beta or higher.",
-              );
-              return;
-            }
-            setSelectedMethod(item);
-          }}
-          children={languages}
-          direction="right"
-          title={languages.find((language: any) => language.id == selectedMethod)?.name}
-        />
-      </div>
-
-      <div className="pr-2 mr-4 pl-4 pt-1 flex flex-row space-x-2">
-        <table className="w-full">
-          <thead className="bg-[#85869822]">
-            <tr className="border-b border-[#4C4F6B]">
-              <th className="text-left pl-2 py-1 font-light rounded-tl-md">File</th>
-              <th className="text-left py-1 font-light ">Description</th>
-              <th className="w-6 rounded-tr-md"></th>
-            </tr>
-          </thead>
-          <tbody className="overflow-y-scroll">
-            <tr>
-              <td className="py-2 pl-2 underline decoration-dotted cursor-pointer" onClick={openModelGenerator}>
-                Model.swift
-              </td>
-              <td className="py-2">Provides the functions that call your backend</td>
-              <td className="py-2 opacity-70 cursor-pointer hover:opacity-100">
-                <FontAwesomeIcon icon={faTrash} />
-              </td>
-            </tr>
-            <tr>
-              <td className="pt-2 pl-2 flex">
-                <div className="w-42">
-                  <Button
-                    text="Add from Figma"
-                    onClick={() => {
-                      authenticateAndOpen();
-                    }}
-                  />
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
       </div>
 
       <FloatingModal
