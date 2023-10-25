ee3df6479030f14e1c92e652b236d32c7f4c2d9d
Scott Bass <scott@galaxysdk.com>
2023-10-25 11:44:54 -0700

    include system vars

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -5,71 +5,73 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  entry: "./src/main.tsx",
-  // devServer: {
-  //   static: path.join(__dirname, "dist"),
-  //   historyApiFallback: true, // this allows routing for react-router
-  //   compress: true,
-  //   port: 3000, // or whatever port you want
-  //   contentBase: path.resolve(__dirname, "public"),
-  // },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.tsx?$/,
-        use: {
-          loader: "ts-loader",
-          options: {
-            transpileOnly: true, // transpile without type checking
-          },
-        },
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      //Workers
-    ],
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "dist"),
+    entry: "./src/main.tsx",
+    // devServer: {
+    //   static: path.join(__dirname, "dist"),
+    //   historyApiFallback: true, // this allows routing for react-router
+    //   compress: true,
+    //   port: 3000, // or whatever port you want
+    //   contentBase: path.resolve(__dirname, "public"),
+    // },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: "/",
     },
-    open: true,
-    port: 8080,
-    historyApiFallback: true,
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
-  },
-  plugins: [
-    new Dotenv(),
-    new HtmlWebpackPlugin({
-      template: "./index.html",
-    }),
-    new MonacoWebpackPlugin({
-      languages: ["json", "javascript", "css", "html", "typescript"],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: "public",
-          to: "",
-          noErrorOnMissing: true,
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+            {
+                test: /\.tsx?$/,
+                use: {
+                    loader: "ts-loader",
+                    options: {
+                        transpileOnly: true, // transpile without type checking
+                    },
+                },
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader", "postcss-loader"],
+            },
+            //Workers
+        ],
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "dist"),
         },
-      ],
-    }),
-  ],
-  stats: { children: true },
+        open: true,
+        port: 8080,
+        historyApiFallback: true,
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    },
+    plugins: [
+        new Dotenv({
+            systemvars: true,
+        }),
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+        }),
+        new MonacoWebpackPlugin({
+            languages: ["json", "javascript", "css", "html", "typescript"],
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: "public",
+                    to: "",
+                    noErrorOnMissing: true,
+                },
+            ],
+        }),
+    ],
+    stats: { children: true },
 };
