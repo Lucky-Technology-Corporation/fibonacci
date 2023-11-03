9e33411f84add61cbbe2abac6e1f57f4b8db6a6d
Adam Barr-Neuwirth <6325943+slugiscool99@users.noreply.github.com>
2023-11-02 18:14:51 -0700

    update initial files

--- src/Utilities/GlobalContext.tsx
+++ src/Utilities/GlobalContext.tsx
@@ -66,8 +66,8 @@ export const GlobalContextProvider = ({ children }) => {
   const [isCreatingProject, setIsCreatingProject] = useState(false);
   const [activeToast, setActiveToast] = useState(null);
   const [postMessage, setPostMessage] = useState(null);
-  const [activeEndpoint, setActiveEndpoint] = useState("backend/user-dependencies/get-.ts");
-  const [activeFile, setActiveFile] = useState("frontend/src/pages/Home.tsx");
+  const [activeEndpoint, setActiveEndpoint] = useState("backend/user-dependencies/get..ts");
+  const [activeFile, setActiveFile] = useState("frontend/src/pages/SwizzleHomePage.tsx");
   const [activeHelper, setActiveHelper] = useState(null);
   const [ideReady, setIdeReady] = useState(false);
   const [testDeployStatus, setTestDeployStatus] = useState("DEPLOYMENT_IN_PROGRESS");
